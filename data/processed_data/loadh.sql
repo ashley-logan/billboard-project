2c5bf07e21dd70619cb6561cb693477fe8748ad0
Ashley Logan <ashley-314159@outlook.com>
2025-07-01 14:01:01 -0400

    hg

--- data/processed_data/loadh.sql
+++ data/processed_data/loadh.sql
@@ -1,44 +1,49 @@
-
-CREATE TEMP TABLE IF NOT EXISTS create_song_hash as (
-    select
+CREATE TEMP
+TABLE IF NOT EXISTS song_base AS (
+    SELECT
         song,
         artists,
-        md5_number_lower(string_agg({date: c.date, pos: c.position}, '|' order by date)) as song_hash,
-        min(date) as debut
-    from charts c,
-    group by all
-
-);
-
-CREATE temp
-TABLE IF NOT EXISTS create_artist_hash AS (
-    SELECT
-        artist,
-        debut,
-        list (
-            song
-            ORDER BY debut
-        ) AS songs,
-        md5_number_lower (
-            string_agg (
-                list (song_hash),
-                '|'
-                ORDER BY debut
-            )
-        ) AS artist_hash,
-    FROM create_song_hash c, unnest (c.artists) AS u (artist)
-    GROUP BY
-        u.artist
-);
-
-CREATE TABLE IF NOT EXISTS new_artists AS (
-    SELECT
         row_number() OVER (
-            ORDER BY debut
-        ) AS artist_id,
-        string_agg (artist, ' and ') AS artist_name,
-        any_value(songs) AS artist_songs
-    FROM create_artist_hash
+            ORDER BY min(DATE)
+        ) AS id_song,
+        min(DATE) AS debut
+    FROM charts c,
     GROUP BY
-        artist_hash
+        song,
+        artists
 );
+
+CREATE TEMP
+TABLE IF NOT EXISTS artist_base AS (
+    SELECT
+        ROW_NUMBER() OVER (
+            ORDER BY ANY_VALUE(art_debut)
+        ) AS id_artist,
+        STRING_AGG (artist, ' and ' ORDER BY id_songs.pos) AS name,
+        LIST (artist ORDER BY id_songs.pos) AS name_comps,
+        id_songs[id] AS song_ids
+    FROM 
+        (SELECT
+            artist,
+            LIST ({'pos': list_indexof(b.artists, u.artist), 'id': id_song}) AS id_songs, 
+            MIN(debut) AS art_debut
+        FROM song_base b, unnest (b.artists) AS u (artist)
+        GROUP BY
+            u.artist)
+        AS by_art
+    GROUP BY
+        id_songs.id
+);
+
+-- CREATE TABLE IF NOT EXISTS new_songs AS (
+--     SELECT
+--         id_song,
+--         ANY_VALUE(song) AS title_song,
+--         LIST (name) AS artist_song,
+--         ANY_VALUE(debut) AS debut_song,
+--     FROM
+--         artist_base ab
+--         JOIN song_base sb ON LIST_CONTAINS (ab.song_ids, sb.id_song)
+--     GROUP BY
+--         id_song
+-- );
