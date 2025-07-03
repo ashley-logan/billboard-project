6c26f8960bf1c3cbfdc26ea7d21b1936e31d1769
Ashley Logan <ashley-314159@outlook.com>
2025-07-02 22:15:06 -0400

    dfd

--- data/processed_data/loadh.sql
+++ data/processed_data/loadh.sql
@@ -13,27 +13,97 @@ TABLE IF NOT EXISTS song_base AS (
         artists
 );
 
-CREATE TEMP
-TABLE IF NOT EXISTS artist_base AS (
+-- CREATE TEMP
+-- TABLE IF NOT EXISTS artist_base AS (
+--     SELECT
+--         ROW_NUMBER() OVER (
+--             ORDER BY ANY_VALUE(art_debut)
+--         ) AS id_artist,
+--         STRING_AGG (
+--             artist,
+--             ' and '
+--             ORDER BY row_pos
+--         ) AS name,
+--         LIST (
+--             artist
+--             ORDER BY row_pos
+--         ) AS name_comps,
+--         first(row_pos),
+--         id_songs AS song_ids
+--     FROM (
+--             SELECT
+--                 artist, any_value(
+--                     list_indexof (b.artists, u.artist)
+--                 ) AS row_pos, LIST (
+--                     id_song
+--                     ORDER BY debut
+--                 ) AS id_songs, MIN(debut) AS art_debut
+--             FROM song_base b, unnest (b.artists) AS u (artist)
+--             GROUP BY
+--                 u.artist
+--         ) AS by_art
+--     GROUP BY
+--         id_songs
+-- );
+
+create temp table if not exists artist_base as (
     SELECT
-        ROW_NUMBER() OVER (
-            ORDER BY ANY_VALUE(art_debut)
-        ) AS id_artist,
-        STRING_AGG (artist, ' and ' ORDER BY id_songs.pos) AS name,
-        LIST (artist ORDER BY id_songs.pos) AS name_comps,
-        id_songs.id AS song_ids
-    FROM 
-        (SELECT
+        artist,
+        list(id_song) as id_songs,
+        first({'song':id_song, 'pos':list_indexof(sb.artists,u.artist)}) as art_order,
+        min(debut) as art_debut
+    FROM song_base sb, unnest(sb.artists) as u(artist)
+    GROUP BY artist
+);
+
+CREATE temp
+TABLE IF NOT EXISTS new_artists AS (
+    SELECT
+        row_number() OVER (
+            ORDER BY first (art_debut)
+        ) AS id,
+        list (
+            artist
+            ORDER BY art_order.pos
+        ) AS name_parts,
+        string_agg (
             artist,
-            row_number() over(PARTITION BY id_song)
-            LIST ({'pos': list_indexof(b.artists, u.artist), 'id': id_song}) AS id_songs, 
-            MIN(debut) AS art_debut
-        FROM song_base b, unnest (b.artists) AS u (artist)
-        GROUP BY
-            u.artist)
-        AS by_art
+            ' and '
+            ORDER BY art_order.pos
+        ) AS name_string,
+        list (art_debut) AS debuts
+    FROM artist_base ab
     GROUP BY
-        id_songs.id
+        id_songs
+);
+
+CREATE temp
+TABLE IF NOT EXISTS new_songs AS (
+    WITH
+        cte AS (
+            SELECT id_song, name_string
+            FROM
+                song_base sb,
+                unnest (sb.artists) AS u (art)
+                JOIN artist_base ab ON LIST_CONTAINS (ab.name_parts, u.art)
+            GROUP BY
+                name_string
+        )
+    SELECT string_agg (
+            name_string, '|'
+            ORDER BY art_debut
+        )
+) CREATE temp
+TABLE IF NOT EXISTS new_songs AS (
+    SELECT id_song AS id, song AS title, list (name_parts) OVER (
+            PARTITION BY
+                art_pos.song
+            ORDER BY art_pos.pos
+        )
+    FROM
+        new_artists na,
+        unnest (ab.art_order) AS u (art_pos)
+        JOIN song_base sb ON art_pos.song = sb.id_song
 );
 
 -- CREATE TABLE IF NOT EXISTS new_songs AS (
@@ -49,8 +119,8 @@ TABLE IF NOT EXISTS artist_base AS (
 --         id_song
 -- );
 
-SELECT row_number() OVER (
-        PARTITION BY
-            id_song
-    ) AS id_list, artist, id_song
-FROM song_base b, unnest (b.artists) AS u (artist);
+-- SELECT row_number() OVER (
+--         PARTITION BY
+--             id_song
+--     ) AS id_list, artist, id_song
+-- FROM song_base b, unnest (b.artists) AS u (artist);
